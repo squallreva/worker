794ef8bf422c51c7c3dd59628f7edd46353418f7
squallreva <95668764+squallreva@users.noreply.github.com>
2025-10-21 17:09:56 +0800

    Update entry.py

--- src/entry.py
+++ src/entry.py
@@ -8,9 +8,12 @@ class Default(WorkerEntrypoint):
             return Response("Method not allowed", status=405)
         if request.headers.get("Content-Type") != "application/json":
             return Response("Content-Type must be application/json", status=400)
-       # body=await request.json()
-       # name=body.get("name")
-       # if not name:
-       #     return Response("Missing 'name' in JSON body", status=400)
+        body=await request.json()
+        name=body.get("name")
+        if not name:
+            return Response("Missing 'name' in JSON body", status=400)
         name2=selft.env.API_HOST
-        return Response(hello(name2))
+        if not name2:
+            return Response("Missing 'name2' in variable", status=400)
+        name =name + name2
+        return Response(hello(name))
